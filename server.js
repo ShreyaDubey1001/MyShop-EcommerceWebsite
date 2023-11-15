3fd8afb941a15f396a990400099f2f3e62940c26
ShreyaDubey1001 <115868572+ShreyaDubey1001@users.noreply.github.com>
2023-11-16 00:14:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import {fileURLToPath} from 'url';
 
 //configure env
 dotenv.config();
 
 //configure db
 connectDB();
+
+//esmodule fix
+const __filename=fileURLToPath(import meta.url);
+const __dirname=path.dirname(__filename);
 
 //rest oject
 const app=express();
